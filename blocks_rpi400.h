abd8e0000e5b27d90f82929fd3dc2cd22ed11860
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2025-06-18 19:09:22 +0100

    added weather_temp program to dwmblocks' block.h.

--- blocks_rpi400.h
+++ blocks_rpi400.h
@@ -17,7 +17,8 @@ static const Block blocks[] = {
 	{"WiFi: ",      "wifi_network", 1,                       0},
 	{"Bluetooth: ", "get_bt_device", 1,                      0},
 	{"",            "get_picom_status", 1,                   0},
-	{"",            "date '+%Y-%m-%d (%a), %H:%M'", 1,       0}
+	{"",            "date '+%Y-%m-%d (%a), %H:%M'", 1,       0},
+    {"",            "weather_temp", 3600,                    0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
